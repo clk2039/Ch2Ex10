e6ac803cf8dbe2a44616aeb0da49c84701010e4d
Fathomice <Fathomice@MOTHERSHIP>
2025-09-14 21:19:34 -0400

    added input functionallity for a user

--- Ch2Ex10.cpp
+++ Ch2Ex10.cpp
@@ -5,7 +5,8 @@ Programmer: Kit Miller
 Date: September 2025
 Requirements: Write a program that calculates how many miles to the gallon a car gets if the car 
 holds 15 gallons of gas and can travel 375 miles before refueling.
-Display the results on the screen.
+Display the results on the screen. 
+This branch is for a user to add input rather than hard coding.
 */
 
 #include <iostream>
@@ -13,14 +14,19 @@ using namespace std;
 
 int main()
 {
-	int gasTank = 15; // gallons of gas the tank can hold
-	int milesToEmpty = 375; // miles the car can travel before refueling
+	int gasTank; // gallons of gas the tank can hold
+	int milesToEmpty; // miles the car can travel before refueling
 	double milesPerGallon; 
+	
+	cout << "Enter the number of gallons your gas tank can hold: ";
+	cin >> gasTank;
+	cout << "Enter the number of miles your car can travel before refueling: ";
+	cin >> milesToEmpty;
 
 	milesPerGallon = milesToEmpty / gasTank;
 
-	cout << "If a car can travel " << milesToEmpty << " miles on " << gasTank 
-		 << " gallons of gas, it gets " << milesPerGallon << " miles per gallon." << endl;
+	cout << "If your car can travel " << milesToEmpty << " miles on " << gasTank 
+		 << " gallons of gas, it gets about " << milesPerGallon << " miles per gallon." << endl;
 
 	return 0;
 }
